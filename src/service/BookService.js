df7f4d3a62f777d32c634e650779d2b3065e4e0c
Ashan Randima Manamperi <120651088+Dettlaff404@users.noreply.github.com>
2025-03-29 16:37:11 +0530

    Handle Service Layer

--- src/service/BookService.js
+++ src/service/BookService.js
@@ -1,19 +1,21 @@
 //CRUD for Book data handling
+const Book = require("../model/BookModel");
 
 async function getAllBooks() {
-    console.log("getAllBooks");
+    return Book.find();
 }
 
-async function addBook() {
-    console.log("addBook");
+async function addBook(book) {
+    const bookData = new Book(book);
+    return bookData.save();
 }
 
-async function deleteBook() {
-    console.log("deleteBook");
+async function deleteBook(bookId) {
+    return Book.findByIdAndDelete(bookId);
 }
 
-async function updateBook() {
-    console.log("updateBook");
+async function updateBook(bookId, book) {
+    return Book.findOneAndUpdate({ bookId: bookId }, book,{ new: true });
 }
 
 module.exports = { getAllBooks, addBook, deleteBook, updateBook }

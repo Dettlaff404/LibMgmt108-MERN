7094b3d5c8662054c54a66c4f77334c5fd597e65
Ashan Randima Manamperi <120651088+Dettlaff404@users.noreply.github.com>
2025-03-29 16:15:59 +0530

    Add mongo model - Books

---
+++ src/model/BookModel.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const {v4: uuidv4} = require("uuid");
+
+const bookSchema = new mongoose.Schema({
+    bookId: {
+        type: String,
+        default: uuidv4,
+        required: true,
+        unique: true
+    },
+    bookName: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    edition: {
+        type: String,
+        required: true
+    },
+    publisher: {
+        type: String,
+        required: true
+    },
+    isbn: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    totalQty: {
+        type: Number
+    },
+    availableQty: {
+        type: Number
+    }
+});
+
+module.exports = mongoose.model("Book", bookSchema);

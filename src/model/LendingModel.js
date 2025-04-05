ada55a20df9c94b59c5fbbc9acc1e1c12d32842f
Ashan Randima Manamperi <120651088+Dettlaff404@users.noreply.github.com>
2025-04-05 10:36:52 +0530

    Updated Lending Model

--- src/model/LendingModel.js
+++ src/model/LendingModel.js
@@ -6,14 +6,14 @@ const lendingSchema = new mongoose.Schema({
     member: { type: String, required: true },
     book: { type: String, required: true },
     lendingDate: { type: String, default: () => new Date().toISOString().split("T")[0] },
-    returnDate: { type: String, required: true, default: ({ lendingDate }) => {
+    returnDate: { type: String, default: ({ lendingDate }) => {
         const date = new Date(lendingDate);
         date.setDate(date.getDate() + 7);
         return date.toISOString().split("T")[0];
     } },
-    isActiveLending: { type: Boolean, required: true },
-    overDueDays: { type: Number, required: false },
-    fineAmount: { type: Number, required: false }
+    isActiveLending: { type: Boolean, default: true },
+    overDueDays: { type: Number, default: 0 },
+    fineAmount: { type: Number, default: 0 }
 });
 
 module.exports = mongoose.model("Lending", lendingSchema);
